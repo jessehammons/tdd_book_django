e28040bdea02df065b64fcac5d01e9543eba8029
peliom <jessehammons@peliom.net>
2019-03-26 21:59:02 -0700

    refactor functional test

--- functional_tests.py
+++ functional_tests.py
@@ -12,6 +12,11 @@ class NewVisitorTest(unittest.TestCase):
 	def tearDown(self):
 		self.browser.quit()
 
+	def check_for_row_in_list_table(self, row_text):
+		table = self.browser.find_element_by_id('id_list_table')
+		rows = table.find_elements_by_tag_name('tr')
+		self.assertIn(row_text, [row.text for row in rows])
+
 	def test_can_start_a_list_and_retrieve_it_later(self):
 		# Edith has heard about a cool new online todo app.  She goes to check out its homepage
 		self.browser.get('http://localhost:8000')
@@ -31,16 +36,19 @@ class NewVisitorTest(unittest.TestCase):
 		# When she hits enter, the page updates, and now the page lists "Buy peacock feathers" as an item in a todo list
 		inputbox.send_keys(Keys.ENTER)
 		time.sleep(1)
-
-		table = self.browser.find_element_by_id('id_list_table')
-		rows = table.find_elements_by_tag_name('tr')
-		expected_row_text = '1: Buy Peacock feathers'
-		self.assertIn(expected_row_text, [row.text for row in rows])
+		self.check_for_row_in_list_table('1: Buy Peacock feathers')
 
 		# There is still a text box inviting her to add another item.  She enters "Use Peacock feathers to make a fly"
-		self.fail('finish the test later')
-
+		inputbox = self.browser.find_element_by_id('id_new_item')
+		inputbox.send_keys('Use peacock feathers to make a fly')
+		inputbox.send_keys(Keys.ENTER)
+		time.sleep(1)
+		
 		# The page updates again, and now shows both items on her list
+		self.check_for_row_in_list_table('1: Buy peacock feathers')
+		self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
+
+		self.fail('finish the test later')
 
 		# Edith wonders whether the site will remember her list.
 		# Then she sees that the site has generated a unique URL for her,
