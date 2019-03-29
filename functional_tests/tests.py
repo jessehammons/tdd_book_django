ded5d7ab60521c17b37948343c6f70bacfe8369a
peliom <jessehammons@peliom.net>
2019-03-29 11:44:12 -0700

    make functional_tests.py into an appdir, use LiveServerTestCase for functional test

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,10 +1,11 @@
+from django.test import LiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
 import time
 import unittest
 
-class NewVisitorTest(unittest.TestCase):
+class NewVisitorTest(LiveServerTestCase):
 
 	def setUp(self):
 		self.browser = webdriver.Firefox()
@@ -19,7 +20,7 @@ class NewVisitorTest(unittest.TestCase):
 
 	def test_can_start_a_list_and_retrieve_it_later(self):
 		# Edith has heard about a cool new online todo app.  She goes to check out its homepage
-		self.browser.get('http://localhost:8000')
+		self.browser.get(self.live_server_url)
 
 		# She notices the page title and header mention todo lists
 		self.assertIn('To-Do', self.browser.title)
@@ -58,5 +59,4 @@ class NewVisitorTest(unittest.TestCase):
 
 		# Satisfied, Edith goes back to sleep
 
-if __name__ == '__main__':
-	unittest.main()
+# no __main__, use python3 manage.py test functional_tests
