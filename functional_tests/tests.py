6b4dff1571e21e0c083eec5866d80231bcadad3f
peliom <jessehammons@peliom.net>
2019-03-29 17:25:56 -0700

    replace time.sleep with wait_for_row_in_list_table

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,9 +1,12 @@
 from django.test import LiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from selenium.common.exceptions import WebDriverException
 
 import time
 import unittest
+
+MAX_WAIT = 10 # seconds to wait for page load
 
 class NewVisitorTest(LiveServerTestCase):
 
@@ -13,10 +16,18 @@ class NewVisitorTest(LiveServerTestCase):
 	def tearDown(self):
 		self.browser.quit()
 
-	def check_for_row_in_list_table(self, row_text):
-		table = self.browser.find_element_by_id('id_list_table')
-		rows = table.find_elements_by_tag_name('tr')
-		self.assertIn(row_text, [row.text for row in rows])
+	def wait_for_row_in_list_table(self, row_text):
+		start_time = time.time()
+		while True:
+			try:
+				table = self.browser.find_element_by_id('id_list_table')
+				rows = table.find_elements_by_tag_name('tr')
+				self.assertIn(row_text, [row.text for row in rows])
+				return
+			except (AssertionError, WebDriverException) as e:
+				if time.time() - start_time > MAX_WAIT:
+					raise e
+				time.sleep(0.5)
 
 	def test_can_start_a_list_and_retrieve_it_later(self):
 		# Edith has heard about a cool new online todo app.  She goes to check out its homepage
@@ -36,18 +47,16 @@ class NewVisitorTest(LiveServerTestCase):
 
 		# When she hits enter, the page updates, and now the page lists "Buy peacock feathers" as an item in a todo list
 		inputbox.send_keys(Keys.ENTER)
-		time.sleep(1)
-		self.check_for_row_in_list_table('1: Buy Peacock feathers')
+		self.wait_for_row_in_list_table('1: Buy Peacock feathers')
 
 		# There is still a text box inviting her to add another item.  She enters "Use Peacock feathers to make a fly"
 		inputbox = self.browser.find_element_by_id('id_new_item')
 		inputbox.send_keys('Use peacock feathers to make a fly')
 		inputbox.send_keys(Keys.ENTER)
-		time.sleep(1)
 		
 		# The page updates again, and now shows both items on her list
-		self.check_for_row_in_list_table('1: Buy Peacock feathers')
-		self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
+		self.wait_for_row_in_list_table('1: Buy Peacock feathers')
+		self.wait_for_row_in_list_table('2: Use peacock feathers to make a fly')
 
 		self.fail('finish the test later')
 
