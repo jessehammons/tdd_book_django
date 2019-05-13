e7db0b5d7736b9bf7ef8e01c495953c9d70e34ae
peliom <jessehammons@peliom.net>
2019-05-13 13:25:55 -0700

    custom passwordless auth backend + custom user model

--- superlists/settings.py
+++ superlists/settings.py
@@ -47,6 +47,9 @@ INSTALLED_APPS = [
 ]
 
 AUTH_USER_MODEL = 'accounts.User'
+AUTHENTICATION_BACKENDS = [
+	'accounts.authentication.PasswordlessAuthenticationBackend',
+]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -125,6 +128,11 @@ LOGGING = {
 	'root': {'level': 'INFO'},
 }
 
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'info@gamedeckapp.com'
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASSWORD')
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.11/topics/i18n/
